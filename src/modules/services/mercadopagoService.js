ee36ac9a19eca47af16c5c4c475244432fec7a96
marcosr1 <marcosrichelly43@gmail.com>
2026-03-18 23:50:38 -0300

    k

--- src/modules/services/mercadopagoService.js
+++ src/modules/services/mercadopagoService.js
@@ -1,12 +1,10 @@
-import { MercadoPagoConfig, Payment } from "mercadopago";
-
-console.log("TOKEN:", process.env.MP_ACCESS_TOKEN);
+import { MercadoPagoConfig, Payment } from "mercadopago"; 
 
 export const createPixPagamento = async ({ amount }) => {
 
     const client = new MercadoPagoConfig({
-        accessToken: process.env.ACCESS_TOKEN
-    })
+        accessToken: process.env.MP_ACCESS_TOKEN
+    });
 
     const payment = new Payment(client);
 
